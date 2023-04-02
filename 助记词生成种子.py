90de0d593c37003cb720e8c2f089acc2ceaf366d
8039763324 <119274156+8039763324@users.noreply.github.com>
2023-04-02 10:54:39 +0800

    Update 助记词生成种子.py

--- 助记词生成种子.py
+++ 助记词生成种子.py
@@ -3,4 +3,4 @@ import binascii
 mnemonicSentence = b'patch purpose fat march slow tiny quarter age spring orphan twin wish'  #助记词
 passphrase = b'mnemonic'+b''	#passphrase,mneonic为默认，passphrase 填入后面 b''
 seed = hashlib.pbkdf2_hmac('sha512', mnemonicSentence, passphrase, 2048,64)		#把助记词 + passphrase 用pbkdf2 用 sha512 2048 次后得出一个128位hex
-print(binascii.hexlify(seed))
+print(binascii.hexlify(seed).decode())
